2077fc77d0300facb12a1c212d181a0e11cce611
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-09-16 03:58:04 +0530

    Add partytown integration for gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://osd.opensourceweekend.org",
   vite: {
     // @ts-ignore
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   server: {
     allowedHosts: [
-      '.ngrok-free.app',   // allow all ngrok domains
-    ]
+      ".ngrok-free.app", // allow all ngrok domains
+    ],
   },
-
-
-  integrations: [react()]
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
